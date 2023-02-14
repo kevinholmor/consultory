056cec01d2051d3bc200f0968d97c4bb044f9f33
Kevin Holmes <22670020@iguala.tecnm.mx>
2023-02-14 07:21:13 -0600

    Se creo la clase Guitar

---
+++ src/Guitar.java
@@ -0,0 +1,15 @@
+public class Guitar {
+    private String serialNumber;
+
+    private double price;
+
+    private String builder;
+
+    private String model;
+
+    private String type;
+
+    private String backWood;
+
+    private String topWood;
+}
